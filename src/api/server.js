2a3a4e5e1dc07f18cc08b1ed1df46583f1c90198
Hossein Heydari <hoseynh30@gmail.com>
2025-12-29 13:32:20 +0330

    f

--- src/api/server.js
+++ src/api/server.js
@@ -20,6 +20,7 @@ import fs from 'fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 export class APIServer {
     constructor(telegramManager, database, monitoringService, operationQueue, config) {
         this.app = express();
